01c34cf494c1075efe1eb217adea0c03710f91d6
Gaceusz <inglorion@op.pl>
2017-05-05 15:06:59 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 var a = prompt('Podaj liczbę a');
 var b = prompt('Podaj liczbę b');
 
-var value=(a * a) + (2 * a * b) - (b * b);
+var value = (a * a) + (2 * a * b) - (b * b);
 
 alert('Wynik działania (a * a) + (2 * a * b) - (b * b) wynosi ' + value);
 
-if (value<0) {
-	alert('Wynik działania jest ujemny');
-  }
-
-if (value==0) {
+if (value == 0) {
 	alert('Wynik działania to zero');
-  }
+} else {
+  	alert('Wynik działania nie jest równy zero')
+}
 
-if (value>0) {
+if (value < 0 ) {
+	alert('Wynik działania jest ujemny');
+}
+
+if (value > 0) {
 	alert('Wynik działania jest dodatni');
-  }
+}
